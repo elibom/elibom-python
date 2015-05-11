f5a6b602b06ed2682bd7e28f646e88e8278d9423
Alejandro <lariverosc@gmail.com>
2015-05-10 23:01:49 -0500

    Update python versions for 3.4

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.4'
     ]
 )
